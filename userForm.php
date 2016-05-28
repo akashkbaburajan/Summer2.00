78e9ead0719283b8e6bf11a3ac31f873d44895c6
akashkbaburajan <akashkbaburajan@gmail.com>
2016-05-28 19:27:58 +0530

    Update userForm.php
    phone no added, div created

--- userForm.php
+++ userForm.php
@@ -1,27 +1,37 @@
 
 
-
 <?php
 /*view file */
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
 /* @var $model app\models\LoginForm */
-
 use yii\helpers\Html;
 // use yii\bootstrap\ActiveForm;
 use yii\widgets\ActiveForm;
 ?>
+<style>
+.font{
+	font-family: Sans-Ascender;
+	color:GREY;
+}
+</style>
+<h1 class='font'> STUDENT REGISTERATION FORM </h1>
 <?php 
 	if(Yii::$app->session->hasFlash('success'))
-	{
+//	{
 		echo Yii::$app->session->getFlash('success');
-			}
+//			}
 ?>
+<div class="row">
+            <div class="col-lg-5">
 
 <?php $form = ActiveForm::begin(); ?>
+<br>
 <?= $form->field($model,'name'); ?>
 <?= $form->field($model,'email'); ?>
+<?= $form->field($model,'phone'); ?>
 <?= $form->field($model, 'areaofinterest')->textArea(['rows' => 6]) ?>
-
-<?= Html::submitButton('Submit',['class'=>'btn btn-primary']); ?>
+</div>
+</div>
+<?= Html::submitButton('Submit',['class'=>'btn btn-primary submit']); ?>
 <?php ActiveForm::end(); ?>
